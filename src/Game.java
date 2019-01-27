3f60aa51d8de5e9f88c744d347f247552e74bccb
Matheus Notebook <Matheus Notebook@Matheus.local>
2019-01-27 00:27:02 -0300

    Teste

--- src/Game.java
+++ src/Game.java
@@ -21,25 +21,42 @@ public class Game extends Canvas implements Runnable, KeyListener {
     public static Player player;
     public static Enemy enemy;
     public static Ball ball;
-
+    private Thread thread;
+    private boolean rodando;
     public static int pontJog;
+    public int fps = 0;
     public static int pontAdv;
-    public BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB),fundo,linha;//onde vai renderizar
+    public BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB), fundo, linha;//onde vai renderizar
     public static FolhadeImg folha;
-    
+
     public Game() {
         this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
         this.addKeyListener(this);
         folha = new FolhadeImg("../res/sprites.png");
-        player = new Player(WIDTH / 2, HEIGHT - 5);//subtrai 5 pq é a altura ai renderiza de cima
-        enemy = new Enemy(WIDTH / 2, 0);
+        player = new Player(WIDTH / 2 - 20, HEIGHT - 5);//subtrai 5 pq é a altura ai renderiza de cima
+        enemy = new Enemy(WIDTH / 2 - 20, 0);
         ball = new Ball(WIDTH / 2, HEIGHT / 2 - 1);
-        
+
     }
 
+    public synchronized void iniciar(){
+        thread = new Thread(this);
+        rodando = true;
+        thread.start();
+    }
+    
+    public synchronized void parar(){
+        rodando = false;
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
     public static void main(String[] args) {
         Game game = new Game();
-        JFrame frame = new JFrame("Pong");
+        JFrame frame = new JFrame("JOGO PONG");
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(game);// componente canvas
@@ -47,7 +64,7 @@ public class Game extends Canvas implements Runnable, KeyListener {
         frame.setLocationRelativeTo(null);// tem q ser dps do add pq ele tem q calcular dnv as domensoes
         frame.setVisible(true);
 
-        new Thread(game).start();
+        game.iniciar();
     }
 
     public void tick() {
@@ -65,26 +82,30 @@ public class Game extends Canvas implements Runnable, KeyListener {
         Graphics g = layer.getGraphics();
         g.setColor(Color.black);
         g.fillRect(0, 0, WIDTH, HEIGHT);
-        
+
         fundo = folha.getSprite(48, 0, 16, 16);//corta uma imagem aq é como se tivesse uma imagem
-        for (int x = 0; x < WIDTH; x+=16) {
-            for (int y = 0; y < HEIGHT; y+=16) {
+        for (int x = 0; x < WIDTH; x += 16) {
+            for (int y = 0; y < HEIGHT; y += 16) {
                 g.drawImage(fundo, x, y, null);
             }
         }
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.BOLD, 10));
+        g.drawString("Matheus", 50, HEIGHT/2);
         linha = folha.getSprite(0, 8, 1, 4);
         for (int x = 0; x < WIDTH; x++) {
-                g.drawImage(linha, x, HEIGHT/2, null);
-            
+
+            g.drawImage(linha, x, HEIGHT / 2, null);
+
         }
         player.render(g);
         enemy.render(g);
         ball.render(g);
         g.setColor(Color.YELLOW);
         g.setFont(new Font("Arial", Font.BOLD, 10));
-        g.drawString("Pontos: "+pontAdv,20, 20);
-        g.drawString("Pontos: "+pontJog, 20, HEIGHT-20);
-        
+        g.drawString("Pontos: " + pontAdv, 20, 20);
+        g.drawString("Pontos: " + pontJog, 20, HEIGHT - 20);
+
         g = bs.getDrawGraphics();
         g.drawImage(layer, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
         bs.show();//tudo foi renderizado mas sem isso não exibe e isso nao deve ser renderizdo sem um buffreerd image layer
@@ -92,7 +113,7 @@ public class Game extends Canvas implements Runnable, KeyListener {
 
     @Override
     public void run() {
-        while (true) {
+        /*while (true) {// Game looping toruxa
             tick();
             render();
             try {
@@ -103,7 +124,31 @@ public class Game extends Canvas implements Runnable, KeyListener {
                 e.printStackTrace();
             }
 
+        }*/
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;//divide um segundo no formato de nanopor amount of ticks FPS serve pra calcular o momemnto do update
+        double delta = 0;
+        int frames = 0; //teste
+        double relogio = System.currentTimeMillis();
+        while (rodando) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;//tempo atual menos ultima vez
+            lastTime = now;
+            if (delta >= 1) {
+                //rodar o jogo
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+            if (System.currentTimeMillis() - relogio >= 1000) {
+                fps = frames;
+                frames = 0;
+                relogio += 1000;
+            }
         }
+        parar();
     }
 
     @Override
